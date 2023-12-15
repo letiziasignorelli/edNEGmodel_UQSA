e28daae07696fdc0ea4972a1d7c44586f74686ea
letiziasignorelli <letizia.signorelli@mail.polimi.it>
2023-12-15 17:44:40 +0100

    Added run_all and plot_all

---
+++ figures/plot_all.sh
@@ -0,0 +1,10 @@
+echo "Plot all figures and print Table.."
+python plot_Figure2.py
+python plot_Figure3.py
+python plot_Figure4.py
+python plot_Figure5.py
+python plot_Figure6.py
+python plot_Figure7.py
+python plot_Figure8.py
+python Table3.py
+echo "Figures done."
---
+++ run_all.sh
@@ -0,0 +1,19 @@
+echo "Creating data for Figure 2..."
+python Figure2.py
+echo "Figure 2 done."
+
+echo "Creating data for Figure 3 and 4..."
+python Figure3_4.py
+echo "Figure 3 and 4 done."
+
+echo "Creating data for Figure 5..."
+python Figure5.py
+echo "Figure 5 done."
+
+echo "Creating data for Figure 6 and 7..."
+python Figure6_7.py
+echo "Figure 6 and 7 done."
+
+echo "Creating data for Figure 8..."
+python Figure8.py
+echo "Figure 8 done."
